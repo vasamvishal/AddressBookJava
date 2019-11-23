3bb9fd5ef7957006e992d57b4daf0792ff447555
dhanashri09 <dhanashri0912@gmail.com>
2019-11-23 11:11:36 +0530

    Added emailverification and testing

---
+++ main/java/com/dummytesting/RegistrationOfUser.java
@@ -0,0 +1,17 @@
+package com.dummytesting;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegistrationOfUser {
+    private String emailvalidatorname = "^[a-zA-Z]{1,}[0-9]*([._+-]{1}[a-zA-Z0-9]+){0,1}[@][a-zA-Z0-9]+[.][a-z]{2,4}([.][a-z]{2}){0,1}$";
+    public Boolean validate_Email_Id(String email) {
+        Pattern emailvalidator = Pattern.compile(emailvalidatorname);
+        Matcher match = emailvalidator.matcher(email);
+        System.out.println(emailvalidatorname);
+        Boolean check = match.matches();
+        System.out.println(check);
+        return check;
+
+    }
+}
